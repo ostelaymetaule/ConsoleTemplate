9a64638f77f05babf53701aa80b4bf49f0ebe738
Alexander Elgin <alexander.elgin@hotmail.com>
2021-02-03 00:04:13 +0100

    Wrapper logging and container configuration

---
+++ src/ConsoleTemplate/App.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleTemplate
+{
+    public class App
+    {
+        private ILogger<App> _logger;
+        private IConfigurationRoot _config;
+        public App(ILogger<App> logger, IConfigurationRoot config)
+        {
+            this._config = config ?? throw new NullReferenceException(nameof(config));
+            this._logger = logger ?? throw new NullReferenceException(nameof(logger));
+        }
+        public Task Run()
+        {
+            System.Console.WriteLine("helloworld programmversion " + _config.GetSection("appVersion").Value);
+            return Task.CompletedTask;
+        }
+
+    }
+}
--- src/ConsoleTemplate/Program.cs
+++ src/ConsoleTemplate/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using Autofac;
+using Serilog;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Autofac.Extensions.DependencyInjection;
+using ConsoleTemplate.Configuration;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace ConsoleTemplate
 {
     class Program
     {
-        static void Main(string[] args)
+        public static IConfigurationRoot configuration;
+        private static IContainer Container { get; set; }
+
+
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //add wrapper logging as shown in https://blog.bitscry.com/2017/05/30/appsettings-json-in-net-core-console-app/
+            // Initialize serilog logger
+            Log.Logger = new LoggerConfiguration()
+                 .WriteTo.Console(Serilog.Events.LogEventLevel.Debug)
+                 .MinimumLevel.Debug()
+                 .Enrich.FromLogContext()
+                 .CreateLogger();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+            var serviceProvider = ContainerConfiguration.Configure(serviceCollection);
+
+
+            try
+            {
+                Log.Information("Starting service");
+                await serviceProvider.GetService<App>().Run();
+                Log.Information("Ending service");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Error running service");
+                throw ex;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+              
+            
         }
     }
 }
